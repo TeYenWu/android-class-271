7ff9ca803847525a22dd1f9385157d2dbb8ca659
Wuteyan <killerw74@gmail.com>
2016-07-26 11:16:15 +0800

    add edittext and radiogroup

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+
+    String sex = "male";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.maleRadioButton)
+                {
+                    sex = "male";
+                }
+                else if(checkedId == R.id.femaleRadioButton)
+                {
+                    sex = "female";
+                }
+            }
+        });
 
     }
 
     public void submit(View view)
     {
-        textView.setText("Hello Android");
+        String text = editText.getText().toString();
+        text = text + " 性別: " + sex;
+        textView.setText(text);
+        editText.setText("");
     }
 }
