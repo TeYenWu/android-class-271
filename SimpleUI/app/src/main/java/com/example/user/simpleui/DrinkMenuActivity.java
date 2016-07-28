1277aa0647d984c427fc8d3db561e1176e8c605a
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-28 11:49:38 +0800

    add total function

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -3,6 +3,8 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,6 +24,8 @@ public class DrinkMenuActivity extends AppCompatActivity {
     int[] images = new int[]{R.drawable.drink1, R.drawable.drink4, R.drawable.drink3, R.drawable.drink2};
 
     List<Drink> drinkList = new ArrayList<>();
+
+    List<Drink> drinkOrderList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +59,26 @@ public class DrinkMenuActivity extends AppCompatActivity {
     {
         DrinkAdapter adapter = new DrinkAdapter(this, drinkList);
         drinkMenuListView.setAdapter(adapter);
+
+        drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Drink drink = (Drink)parent.getAdapter().getItem(position);
+                drinkOrderList.add(drink);
+                setupTotalTextView();
+            }
+        });
+    }
+
+    public void setupTotalTextView()
+    {
+        int total = 0;
+        for (Drink drink :drinkOrderList)
+        {
+            total += drink.mPrice;
+        }
+
+        totalTextView.setText(String.valueOf(total));
     }
 
     @Override
