1fa5bed00270f1fc9a0d62b504b7fe751f973054
FrankLikuohao <frank.likuohao@gmail.com>
2016-08-05 12:24:18 +0800

    add route from current location

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,9 +1,25 @@
 package com.example.user.simpleui;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.location.LocationListener;
+
+import android.graphics.Color;
+import android.graphics.Point;
+import android.location.Location;
 import android.media.Image;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +30,10 @@ import org.w3c.dom.Text;
 
 import android.os.Handler;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,13 +41,23 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.LogRecord;
 
-public class OrderDetailActivity extends AppCompatActivity implements GeoCodingTask.GeoCodingResponse{
+public class OrderDetailActivity extends AppCompatActivity implements GeoCodingTask.GeoCodingResponse, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, RoutingListener {
+
+    final static int ACCESS_FINE_LOCATION_REQUEST_CODE = 0;
 
     GoogleMap map;
+    LatLng storeLocation;
+
+    GoogleApiClient googleApiClient;
+
+    LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +107,119 @@ public class OrderDetailActivity extends AppCompatActivity implements GeoCodingT
         MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("NTU").snippet("Hello Marker");
         map.moveCamera(cameraUpdate);
         map.addMarker(markerOptions);
+
+        storeLocation = latLng;
+
+        createGoogleApiClient();
+    }
+
+    private void createGoogleApiClient()
+    {
+        if(googleApiClient == null)
+        {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                            .addConnectionCallbacks(this)
+                            .addOnConnectionFailedListener(this)
+                            .addApi(LocationServices.API)
+                            .build();
+            googleApiClient.connect();
+
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M)
+            {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_REQUEST_CODE);
+            }
+            return;
+        }
+
+        if(locationRequest == null)
+        {
+            locationRequest = new LocationRequest();
+            locationRequest.setInterval(5000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        LatLng start = new LatLng(25.0206, 121.534);
+        if(location != null)
+            start = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 17));
+
+        Routing routing = new Routing.Builder()
+                .travelMode(AbstractRouting.TravelMode.WALKING)
+                .alternativeRoutes(false)
+                .waypoints(start, storeLocation)
+                .withListener(this)
+                .build();
+
+        routing.execute();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == ACCESS_FINE_LOCATION_REQUEST_CODE)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                onConnected(null);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> arrayList, int i) {
+        Route route = arrayList.get(i);
+
+        List<LatLng> points = route.getPoints();
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+
+        polylineOptions.addAll(points);
+
+        polylineOptions.color(Color.GREEN);
+        polylineOptions.width(10);
+
+        map.addPolyline(polylineOptions);
+    }
+
+    @Override
+    public void onRoutingCancelled() {
 
     }
 //
